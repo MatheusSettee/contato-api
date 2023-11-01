31785e1a17f6c086a20494de769454707b01ace4
MatheusSettee <matheussette09@gmail.com>
2023-11-01 14:13:58 -0300

    feat: Iniciando o projeto

---
+++ src/main/java/br/com/msf/appcontatos/apicontatos/ApiContatosApplication.java
@@ -0,0 +1,15 @@
+package br.com.msf.appcontatos.apicontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ApiContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContatosApplication.class, args);
+	}
+
+}
