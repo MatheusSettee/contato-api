548fabf2b7a49a1d2abd2f8f5065804ebc896bf7
MatheusSettee <matheussette09@gmail.com>
2023-11-01 14:15:04 -0300

    feat: criadas as entidades pessoa e contato

---
+++ src/main/java/br/com/msf/appcontatos/apicontatos/entity/Contato.java
@@ -0,0 +1,73 @@
+package br.com.msf.appcontatos.apicontatos.entity;
+
+import br.com.msf.appcontatos.apicontatos.model.enums.Tipo;
+import jakarta.persistence.*;
+
+@Entity
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Tipo tipo;
+
+    private String nome;
+    private String numero;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+    public Contato() {}
+    public Contato(Long id, Tipo tipo, String numero, Pessoa pessoa, String nome) {
+        this.id = id;
+        this.tipo = tipo;
+        this.numero = numero;
+        this.pessoa = pessoa;
+        this.nome = nome;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
---
+++ src/main/java/br/com/msf/appcontatos/apicontatos/entity/Pessoa.java
@@ -0,0 +1,97 @@
+package br.com.msf.appcontatos.apicontatos.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String endereco;
+
+    private String cep;
+
+    private String cidade;
+
+    private String uf;
+    @JsonIgnore
+    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Contato> contatos = new ArrayList<>();
+
+    public Pessoa() {}
+
+    public Pessoa(Long id, String nome, String endereco, String cep, String cidade, String uf) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.contatos  = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contato> contatos) {
+        this.contatos = contatos;
+    }
+}
+
+
