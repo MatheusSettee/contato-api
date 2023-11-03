832b66bca299bf3f25609e12a1b858a587dc486f
MatheusSettee <matheussette09@gmail.com>
2023-11-03 15:28:33 -0300

    feat: imports atualizados

--- src/main/java/br/com/msf/appcontatos/apicontatos/repository/ContatoRepository.java
+++ src/main/java/br/com/msf/appcontatos/apicontatos/repository/ContatoRepository.java
@@ -1,6 +1,6 @@
 package br.com.msf.appcontatos.apicontatos.repository;
 
-import br.com.msf.appcontatos.apicontatos.entity.Contato;
+import br.com.msf.appcontatos.apicontatos.model.entity.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/br/com/msf/appcontatos/apicontatos/repository/PessoaRepository.java
+++ src/main/java/br/com/msf/appcontatos/apicontatos/repository/PessoaRepository.java
@@ -1,6 +1,6 @@
 package br.com.msf.appcontatos.apicontatos.repository;
 
-import br.com.msf.appcontatos.apicontatos.entity.Pessoa;
+import br.com.msf.appcontatos.apicontatos.model.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
